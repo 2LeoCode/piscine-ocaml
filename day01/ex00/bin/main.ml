ebc3dd36d9fc98e64927b1b69fe64fd3c925d1f9
Leo SUARDI <lsuardi@f1r10s19.clusters.42paris.fr>
2025-12-12 19:42:03 +0100

    start refactoring day01

--- day01/ex00/bin/main.ml
+++ day01/ex00/bin/main.ml
@@ -1,14 +1,1 @@
-open Ex00.Repeat_x
-
-let ( %> ) f g x = x |> f |> g
-let print_case = repeat_x %> print_endline
-
-let () =
-  -1 |> print_case;
-  print_case 0;
-  print_case 1;
-  print_case 2;
-  print_case 5;
-  print_case 17;
-  print_case 48;
-  print_case 72
+let () = ignore Ex00.Repeat_x.repeat_x
--- day01/ex00/lib/repeat_x.ml
+++ day01/ex00/lib/repeat_x.ml
@@ -23,3 +23,15 @@ let repeat_x n =
   in
 
   if n < 0 then "Error" else repeat_x' "" n
+
+let () =
+  let print_case x = x |> repeat_x |> print_endline in
+
+  print_case (-1);
+  print_case 0;
+  print_case 1;
+  print_case 2;
+  print_case 5;
+  print_case 17;
+  print_case 48;
+  print_case 72
